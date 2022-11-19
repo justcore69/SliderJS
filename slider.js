771994c31b41140896ffeb6ae5b854cfe693cb15
Core <ndreil.l.d.2000@gmail.com>
2022-11-19 18:39:31 +0300

    Added slide buttons

--- slider.js
+++ slider.js
@@ -1,34 +1,54 @@
+//ELEMENTS
 const sliderDiv = document.querySelector('.slider-div')
 const imagesDiv = document.querySelector('.images-div')
 
-//SLIDER WINDOW SIZE
-const canvasWidth = 540;
-const canvasHeight = 360;
+const arrLeft = document.querySelector('.left')
+const arrRight = document.querySelector('.right')
 
+//SLIDER WINDOW SIZE
+const canvasWidth = 540
+const canvasHeight = 360
+
+//DO NOT CHANGE
+let offset = 0
+
+//ARRAY OF IMAGES
 let images = 
-['/images/slide1.jpg', '/images/slide2.jpg', '/images/slide3.jpg', '/images/slide4.jpg']
+['images/slide1.jpg', 'images/slide2.jpg', 'images/slide3.jpg', 'images/slide4.jpg']
 
 displayImages()
 
-sliderDiv.style.width = canvasWidth;
-sliderDiv.style.height = canvasHeight;
+sliderDiv.style.width = canvasWidth
+sliderDiv.style.height = canvasHeight
 
-function displayImages(){
+function displayImages(){//DISPLAY ALL IMAGES FROM images ARRAY
     for(let i = 0; i < images.length; i++){
         let _img = document.createElement('img');
         _img.src = images[i]
 
         _img.style.width = canvasWidth
+        _img.style.height = canvasHeight
 
         imagesDiv.appendChild(_img)
     }
 }
 
-imagesDiv.onclick = function(){
+arrLeft.onclick = function(){//Click on the left button
+    move('left')
+}
+arrRight.onclick = function(){//Click on the right button
     move('right')
 }
 
 function move(_dir){
-    _dir === 'right' ? imagesDiv.style.left = canvasWidth : imagesDiv.style.left = canvasWidth
-    console.log('jj')
+    if(_dir === 'left'){
+        imagesDiv.style.left = offset + canvasWidth
+        offset += canvasWidth
+    }else if(_dir === 'right'){
+        imagesDiv.style.left = offset + -canvasWidth
+        offset += -canvasWidth
+    }
+
+    
+    console.log(offset + ' | ' + _dir)
 }
