399590fd78c5e40145cb86b91f7d3f59210e373a
Core <109795959+JustAnCore@users.noreply.github.com>
2022-11-20 17:46:16 +0300

    Added autoslide

--- slider.js
+++ slider.js
@@ -5,18 +5,36 @@ const imagesDiv = document.querySelector('.images-div')
 const arrLeft = document.querySelector('.left')
 const arrRight = document.querySelector('.right')
 
-//SLIDER WINDOW SIZE
+//SLIDER CANVAS SIZE
 const canvasWidth = 540
 const canvasHeight = 360
+
+//-----SETTINGS-----
+
+let useOverflowHidden = true //Hide images if it's outside the slider container
+
+let useAutoSlide = true //Automatic slide switching
+let autoSlideDelay = 3 //Delay between switching slides in seconds (Don't works if useAutoSlide = false)
+let autoSlideSide = 'right' //'left' or 'right'
+
+let showDots = true; //Show dots under the slider
+
+let slideAnimationSpeed = 1 //Slide animation speed
+
+//------------------
+
+//ARRAY OF IMAGES | the aspect ratio must match the canvas size
+let images = 
+['images/slide1.jpg', 'images/slide2.jpg', 'images/slide3.jpg', 'images/slide4.jpg']
 
 //DO NOT CHANGE
 let offset = 0
 
-//ARRAY OF IMAGES
-let images = 
-['images/slide1.jpg', 'images/slide2.jpg', 'images/slide3.jpg', 'images/slide4.jpg']
-
+syncWithSize()
 displayImages()
+applyCSS()
+
+autoSlide()
 
 sliderDiv.style.width = canvasWidth
 sliderDiv.style.height = canvasHeight
@@ -30,6 +48,29 @@ function displayImages(){//DISPLAY ALL IMAGES FROM images ARRAY
         _img.style.height = canvasHeight
         
         imagesDiv.appendChild(_img)
+    }
+}
+
+function syncWithSize(){//CSS SYNC WITH CANVAS SIZE
+    const arrows = document.querySelectorAll('.arrow-div')
+    arrows.forEach((arr) => {arr.style.height = canvasHeight})
+    const right = document.querySelector('.right')
+    right.style.left = canvasWidth - canvasWidth / 100 * 25
+}
+
+function applyCSS(){
+    if(useOverflowHidden){
+        sliderDiv.style.overflow = 'hidden'
+    }else{
+        sliderDiv.style.overflow = 'visible'
+    }
+}
+
+async function autoSlide(){
+    if(useAutoSlide){
+        move(autoSlideSide)
+        await delay(autoSlideDelay)
+        autoSlide()
     }
 }
 
@@ -62,5 +103,9 @@ function move(_dir){
         }
     }
 
-    console.log(offset + ' | ' + _dir)
+    //console.log(offset + ' | ' + _dir)
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time * 1000));
 }
